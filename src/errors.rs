d4b68921c1a837f07d90376926e68eb231a5b8a9
Gabriel Windlin <gawindlin@gmail.com>
2026-01-20 13:20:21 +0100

    main -> errors

---
+++ src/errors.rs
@@ -0,0 +1,12 @@
+use thiserror::Error;
+use std::io;
+
+#[derive(Error, Debug)]
+pub enum KirokuError {
+    #[error("IO error: {0}")]
+    Io(#[from] io::Error),
+    #[error("Git error: {0}")]
+    Git(String),
+    #[error("Environment error: {0}")]
+    Env(String),
+}
