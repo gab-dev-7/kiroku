d384843aaaeb1bc85500cab24bdcbe5d40d03c1b
Gabriel Windlin <gawindlin@gmail.com>
2026-01-20 13:20:35 +0100

    main -> events

---
+++ src/events.rs
@@ -0,0 +1,29 @@
+use crossterm::event::{self, Event, KeyEvent};
+use std::time::Duration;
+use anyhow::Result;
+
+pub enum AppEvent {
+    Input(KeyEvent),
+    Tick,
+}
+
+pub struct EventHandler {
+    tick_rate: Duration,
+}
+
+impl EventHandler {
+    pub fn new(tick_rate_ms: u64) -> Self {
+        Self {
+            tick_rate: Duration::from_millis(tick_rate_ms),
+        }
+    }
+
+    pub fn next(&self) -> Result<AppEvent> {
+        if event::poll(self.tick_rate)? {
+            if let Event::Key(key) = event::read()? {
+                return Ok(AppEvent::Input(key));
+            }
+        }
+        Ok(AppEvent::Tick)
+    }
+}
